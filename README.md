a5d054f6c0c11a53aff5d0a031e61e7dff29de00
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2025-04-12 16:18:22 +0200

    feat: Add CLI with the pipeline

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # hypergraph-properties
 
-Example flow:
+## Python API:
 
 ```python
 from hypergraph_properties.hg_properties.corr import node_corr
@@ -25,4 +25,25 @@ Output:
 [hypergraph-properties] [info   ] reading file ./data/tags-ask-ubuntu.txt
 [hypergraph-properties] [info   ] reading ./data/tags-ask-ubuntu.txt complete
 [hypergraph-properties] [info   ] node_corr(hg, log_degrees=True) = 0.3954907961556929 (p=5.7688107146332454e-114)
+```
+
+
+## CLI
+
+```commandline
+$ hypergraph-properties ./data/tags-ask-ubuntu.txt --fmt empirical
+```
+
+Output:
+
+```text
+[hypergraph-properties] [info   ] running `hypergraph-properties` pipeline on file ./data/tags-ask-ubuntu.txt
+[hypergraph-properties] [info   ] expecting hypergraph to be in `empirical` format
+[hypergraph-properties] [warning] final report will not be saved
+[hypergraph-properties] [info   ] reading file ./data/tags-ask-ubuntu.txt
+[hypergraph-properties] [info   ] reading ./data/tags-ask-ubuntu.txt complete
+[hypergraph-properties] [info   ] corr = node_corr(hg, log_degrees=True, log_avg_he_sizes=True) = 0.41230378723158423 (p=1.187410149783796e-124)
+[hypergraph-properties] [info   ] corr = node_corr(hg, log_degrees=True, log_avg_he_sizes=False) = 0.3954907961556929 (p=5.7688107146332454e-114)
+[hypergraph-properties] [info   ] corr = node_corr(hg, log_degrees=False, log_avg_he_sizes=True) = 0.1662450181861485 (p=3.264299752282773e-20)
+[hypergraph-properties] [info   ] corr = node_corr(hg, log_degrees=False, log_avg_he_sizes=False) = 0.1811359746915786 (p=9.358505850238468e-24)
 ```
--- src/hypergraph_properties/__init__.py
+++ src/hypergraph_properties/__init__.py
@@ -1,1 +1,43 @@
+import click
 
+from hypergraph_properties.hg_properties.corr import node_corr
+from hypergraph_properties.hg_reader.readers import EmpiricalHGReader, SyntheticHGReader
+from hypergraph_properties.hg_reader.template import HypergraphReader
+from hypergraph_properties.utils.logger import get_logger
+
+logger = get_logger()
+
+
+@click.command()
+@click.argument("filename", type=click.Path(exists=True))
+@click.option(
+    "--fmt", type=click.Choice(["empirical", "synthetic", "hgf"], case_sensitive=False)
+)
+@click.option(
+    "--out", type=click.Path(writable=True), default=None, help="Output file path."
+)
+def main(filename: click.Path, fmt: str, out: click.Path | None) -> None:
+    logger.info(f"running `hypergraph-properties` pipeline on file {filename}")
+    logger.info(f"expecting hypergraph to be in `{fmt}` format")
+
+    if out is not None:
+        logger.info(f"final report will be saved to {out}")
+    else:
+        logger.warning("final report will not be saved")
+
+    reader: HypergraphReader = {
+        "empirical": EmpiricalHGReader,
+        "synthetic": SyntheticHGReader,
+    }.get(fmt, EmpiricalHGReader)()
+
+    hg = reader.read_graph(filename)
+
+    _ = out  # TODO: save to file
+
+    for log_degrees in [True, False]:
+        for log_avg_he_sizes in [True, False]:
+            corr = node_corr(
+                hg, log_degrees=log_degrees, log_avg_he_sizes=log_avg_he_sizes
+            )
+            expr = f"corr = node_corr(hg, log_degrees={log_degrees}, log_avg_he_sizes={log_avg_he_sizes})"
+            logger.info(f"{expr} = {corr.statistic} (p={corr.pvalue})")

f8f157431c5bee85ee500e8b80467ab261a4f302
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2025-04-12 18:11:04 +0200

    chore: update the corr algorithm

--- src/hypergraph_properties/hg_properties/corr.py
+++ src/hypergraph_properties/hg_properties/corr.py
@@ -2,7 +2,7 @@ import numpy as np
 from numba import njit
 from numpy.typing import NDArray
 from scipy import stats
-from scipy.sparse import csr_array
+from scipy.stats._result_classes import PearsonRResult
 
 from hypergraph_properties.hg_model import Hypergraph
 
@@ -11,26 +11,26 @@ def node_corr(
     hg: Hypergraph,
     log_degrees: bool = False,
     log_avg_he_sizes: bool = False,
-) -> np.float64:
+) -> PearsonRResult:
     vertex_indices = np.array([v - 1 for v in hg.vertex_meta], dtype=np.int32)
     matrix = hg.matrix
 
     degrees = row_sums(matrix.indptr, matrix.data, matrix.shape[0])[vertex_indices]
     if log_degrees:
-        degrees = np.log(degrees) + 1
+        degrees = np.log(degrees + 1)
 
     avg_he_sizes = compute_avg_he_sizes(
         vertex_indices, matrix.indptr, matrix.indices, matrix.data, *matrix.shape
     )
 
     if log_avg_he_sizes:
-        avg_he_sizes = np.log(avg_he_sizes) + 1
+        avg_he_sizes = np.log(avg_he_sizes + 1)
 
     return stats.pearsonr(degrees, avg_he_sizes)
 
 
 @njit
-def row_sums(indptr, data, n_rows) -> NDArray[np.int64]:
+def row_sums(indptr: NDArray[np.int32], data: NDArray[np.bool], n_rows: int) -> NDArray[np.int64]:
     result = np.zeros(n_rows, dtype=np.float64)
     for i in range(n_rows):
         result[i] = np.sum(data[indptr[i] : indptr[i + 1]])
@@ -38,7 +38,14 @@ def row_sums(indptr, data, n_rows) -> NDArray[np.int64]:
 
 
 @njit
-def compute_avg_he_sizes(vertex_indices, indptr, indices, data, n_rows, n_cols):
+def compute_avg_he_sizes(
+        vertex_indices: NDArray[np.int32],
+        indptr: NDArray[np.int32],
+        indices: NDArray[np.int32],
+        data: NDArray[np.bool],
+        n_rows: int,
+        n_cols: int,
+) -> NDArray[np.int64]:
     avg_sizes = np.zeros(len(vertex_indices), dtype=np.float64)
 
     col_degrees = np.zeros(n_cols, dtype=np.float64)
@@ -49,7 +56,7 @@ def compute_avg_he_sizes(vertex_indices, indptr, indices, data, n_rows, n_cols):
 
     for idx, v in enumerate(vertex_indices):
         start = indptr[v]
-        end = indptr[v + 1]
+        end = indptr[v + np.int32(1)]
         if start == end:
             avg_sizes[idx] = 0.0
             continue
@@ -62,11 +69,3 @@ def compute_avg_he_sizes(vertex_indices, indptr, indices, data, n_rows, n_cols):
         avg_sizes[idx] = total / (end - start)
 
     return avg_sizes
-
-
-def vertex_degree(matrix: csr_array, vertex: int) -> np.int64:
-    return matrix[vertex].sum()
-
-
-def avg_he_size(matrix: csr_array, vertex: int) -> np.float64:
-    return matrix[:, matrix[vertex]].sum() / vertex_degree(matrix, vertex)

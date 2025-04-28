370f5ed51bebb55e515251abad48f1ca945adc3a
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2025-04-28 17:05:58 +0200

    test case for singleton removal

--- tests/unit/test_corr.py
+++ tests/unit/test_corr.py
@@ -2,7 +2,8 @@ import numpy as np
 import pytest
 from scipy.sparse import csr_array
 
-from hypergraph_properties.hg_properties.corr import compute_vertex_degrees, compute_avg_he_sizes
+from hypergraph_properties.hg_properties.corr import compute_vertex_degrees, compute_avg_he_sizes, \
+    with_removed_singleton_vertices
 
 
 @pytest.fixture(scope="session")
@@ -32,3 +33,17 @@ def test_compute_avg_he_sizes(matrix) -> None:
         (3 + 4) / 2,
         (3 + 4) / 2,
     ]))
+
+
+def test_removing_singleton_vertices(matrix) -> None:
+    # insert a singleton vertex and assert that it's there
+    matrix_with_singleton = csr_array(np.vstack([matrix.toarray(), [0, 0, 0, 0]]))
+
+    assert 0 in matrix_with_singleton.sum(axis=1)
+
+    matrix_no_singleton = with_removed_singleton_vertices(matrix_with_singleton)
+
+    # no singleton vertices in the result matrix
+    assert not np.any(matrix_no_singleton.sum(axis=1) == 0)
+
+    assert matrix_no_singleton.shape[0] == matrix_with_singleton.shape[0] - 1
